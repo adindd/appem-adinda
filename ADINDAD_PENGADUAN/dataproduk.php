69a43f98ebf234de5ff436f5114f3e175f376175
adinda <111264781+adindd@users.noreply.github.com>
2023-12-04 20:31:37 +0700

    Create dataproduk.php

---
+++ ADINDAD_PENGADUAN/dataproduk.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+
+<style>
+.buat
+{ 
+    font-size: 30px;
+    text-decoration: none;
+    color: black;
+    font-family: ;
+    text-align: left;
+}
+h1{
+  font-size: 30px;
+    text-decoration: none;
+    color: black;
+    font-family:;
+    text-align: center;
+
+}
+body
+{
+    background-color: ;
+
+}
+
+
+</style>
+
+<body>
+
+<div class="produk">
+    <a class="buat" href="{{ ('isi_laporan') }} ">+</a>
+    <h1><center><b><big>DATA PRODUK</big></b></center></h1>
+  <div class="container">
+  <table class="table table-primary">
+  <thead>
+    <tr>
+      <th scope="col">id</th>
+      <th scope="col">Tanggal Kejadian</th>
+      <th scope="col">Nik</th>
+      <th scope="col">Isi Laporan</th>
+      <th scope="col">Foto</th>
+      <th scope="col">Status</th>
+      <th scope="col">Opsi</th>
+    </tr>
+  </thead>
+  
+</table>
+    
+    
+</nav>
+</body>
+</html>
