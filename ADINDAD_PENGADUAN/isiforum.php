07201b1ba03063d8f8ead57f894defcac4ea1eff
adinda <111264781+adindd@users.noreply.github.com>
2023-12-05 21:35:54 +0700

    Create isiforum.php

---
+++ ADINDAD_PENGADUAN/isiforum.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <title>Document</title>
+<head>
+    <center>
+        <br>
+    <h1>Tambah Produk</h1>
+    <br>
+</head>
+<body>
+    
+    <form action="" method="POST">
+        <p>Nama Produk <input type="text" name="Nama produk" required/></p>
+        <p>Harga <input type="text" name="Harga" required/></p>
+        <p>Stok <input type="text" name="Stok" required/></p>
+        <button type="button" class="btn btn-outline-success">Kirim</button>
+    </form>
+    </center>
+</body>
+</html>
+</body>
+</html>
